e4a9af08e03a6e93fe89d4756e590c287c138a43
John Roll <john@rkroll.com>
2023-07-25 14:10:56 -0400

    subscription debug

---
+++ client/stacktrace.tcl
@@ -0,0 +1,18 @@
+# https://wiki.tcl-lang.org/page/List+the+call+stack
+
+proc stacktrace {} {
+    set stack "Stack trace:\n"
+    for {set i 1} {$i < [info level]} {incr i} {
+        set lvl [info level -$i]
+        set pname [lindex $lvl 0]
+        append stack [string repeat " " $i]$pname
+        foreach value [lrange $lvl 1 end] arg [info args $pname] {
+            if {$value eq ""} {
+                info default $pname $arg value
+            }
+            append stack " $arg='$value'"
+        }
+        append stack \n
+    }
+    return $stack
+}
