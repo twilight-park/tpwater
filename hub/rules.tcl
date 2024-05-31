5bc063688c35ff09e123b454b17cdd67c5aa66fc
John Roll <john@rkroll.com>
2024-05-31 12:54:25 -0400

    adjust flow scale and enable third level auto rule

--- hub/rules.tcl
+++ hub/rules.tcl
@@ -4,11 +4,11 @@ every 5000 {
         if { $::auto } {
             if { $::tank <= 100 } {
                 set ::golf:request 1
-                # set ::thrd:request 1
+                set ::thrd:request 1
             }
             if { $::tank >= 102 } {
                 set ::golf:request 0
-                # set ::thrd:request 0
+                set ::thrd:request 0
             }
         }
     } on error msg {
