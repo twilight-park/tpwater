814b74e48557097c457f405486572b280e0ba5ee
John Roll <john@rkroll.com>
2024-09-07 13:09:40 -0400

    Replace Chartist with uPlot

---
+++ share/html/css.m
@@ -0,0 +1,55 @@
+  <style type="text/css">
+    .chart#flowChart {
+        height: 50%;
+        width: 100%;
+    }
+    .chartTitle {
+        margin: 5px;
+    }
+    .ct-series-a .ct-line {
+        stroke: blue;
+        stroke-width: 2;
+    }
+    .tophdr {
+        display: inline;
+        position: fixed;
+        width: 95%
+    }
+    .tophdr h4 {
+        display: inline;
+    }
+    .rite {
+        display: inline;
+        float: right
+    }
+
+    .box {
+        display: inline;
+        position: fixed;
+        width: 95%;
+        padding-top: 5vh;
+    }
+    .box h1 {
+        display: inline;
+        font-weight: bold;
+        font-size: 40px;
+        width: 95%;
+        padding-left: 5vw;
+    }
+
+    .buthdr {
+        display: inline;
+    }
+    div.buttons {
+        display: flex;
+        justify-content: space-around;
+        position: fixed;
+        bottom: 15%;
+        width: 100%;
+    }
+    .buthdr button {
+        display: inline;
+        font-weight: bold;
+        font-size: 125%;
+    }
+  </style>
